e37d745b3f83f0c3ee40c14c0cd47a63a422badc
John Hitz <johnh@whodapet.com>
2021-01-21 20:48:02 -0600

    All test passing

--- lib/protein_translation.ex
+++ lib/protein_translation.ex
@@ -1,4 +1,7 @@
 defmodule ProteinTranslation do
+
+  @codons ["UGU", "UGC", "UUA", "UUG", "AUG", "UUU", "UUC", "UCU", "UCC", "UCA", "UCG", "UGG", "UAU", "UAC", "UAA", "UAG", "UGA"]
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
@@ -6,21 +9,28 @@ defmodule ProteinTranslation do
   def of_rna(rna) do
     list = for <<x::binary-3 <- rna>>, do: x
 
-    list =
-      list
-      |> Enum.reduce_while([], fn x, acc ->
-        if x not in ["UAA", "UAG", "UGA"] do
-          {:ok, protein} = ProteinTranslation.of_codon(x)
-          IO.puts("protein: #{protein}")
-          {:cont, acc ++ [protein]}
-        else
-          {:halt, acc}
-        end
-      end)
+    rna_in = Enum.all?(list, fn x ->
+      Enum.member?(@codons, x)
+    end)
 
-    IO.puts("list is: #{list}")
-
-    {:ok, list}
+    result = Enum.reduce(list, {}, fn x, _result ->
+      case x do
+        x when rna_in == true ->
+          list =
+            list
+            |> Enum.reduce_while([], fn x, acc ->
+              if x not in ["UAA", "UAG", "UGA"] do
+                {:ok, protein} = ProteinTranslation.of_codon(x)
+                {:cont, acc ++ [protein]}
+              else
+                {:halt, acc}
+              end
+            end)
+          _result = {:ok, list}
+        _ ->
+          _result = {:error, "invalid RNA"}
+      end
+    end)
   end
 
   @doc """
@@ -46,6 +56,7 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
+    length = String.length(codon)
     case codon do
       "UGU" -> {:ok, "Cysteine"}
       "UGC" -> {:ok, "Cysteine"}
@@ -64,7 +75,9 @@ defmodule ProteinTranslation do
       "UAA" -> {:ok, "STOP"}
       "UAG" -> {:ok, "STOP"}
       "UGA" -> {:ok, "STOP"}
-      _ -> {:error, "invalid codon"}
+      codon when length != 3 ->
+        {:error, "invalid codon"}
+      # _ ->_result = {:error, "invalid RNA"}
     end
   end
 end
